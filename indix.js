0aed2d5cc6e24527bf4a10e75f2b9dd53f22b4f1
Asiksaikh <asiksaikh1999@gmail.com>
2025-02-14 23:50:38 +0530

    this code add this new line

--- indix.js
+++ indix.js
@@ -7,3 +7,10 @@ document.write(" This the name type="+typeof(name));
 document.write("<br>");
 var num1=23;
 document.write(" this is the num="+typeof(num1));
+console.log("this is the my code of this code in the console </br>")
+document.write("</br>");
+document.write("this is my"+"code in the name of the file"+"my name is asil "+"saik this the code of the file is over");
+document.write("my name is asik  saikh this is the code of the file is over");
+document.write("<br>");
+var name1=" this is my cuntry name india";
+document.write("thic code nuumor of String="+name1.length);
