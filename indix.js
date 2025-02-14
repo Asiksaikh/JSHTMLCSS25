52f552068037d5ae11dc3fb9159f2c4aec76e1e8
Asiksaikh <asiksaikh1999@gmail.com>
2025-02-14 23:18:30 +0530

    add the new line

---
+++ indix.js
@@ -0,0 +1,9 @@
+document.write("this is the first line of code in the file");
+alert("this is the second line of code in the fike that will be displayed in an alert box");
+document.write("<br>");
+var name="asik";
+// console.long(typeof(name));
+document.write(" This the name type="+typeof(name));
+document.write("<br>");
+var num1=23;
+document.write(" this is the num="+typeof(num1));
